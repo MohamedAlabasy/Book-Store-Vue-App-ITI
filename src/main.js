24c3afdd5289e691729558542ed3d5e3c6aea2bd
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-04-26 14:57:07 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+import mainComponent from './mainComponent.vue'
+
+createApp(mainComponent).mount('#app')
---
+++ src/mainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <booksComponent />
+</template>
+
+<script>
+import booksComponent from "./components/booksComponent.vue";
+
+export default {
+  data: () => ({}),
+  methods: {},
+  computed: {},
+  components: {
+    booksComponent,
+  },
+};
+</script>
